be8933d0cc07c70c387e3802896a40fb593cde22
christopherfricke <christopher.fricke@bmoregeo.com>
2015-03-14 13:51:55 -0400

    Add in map from leaflet

--- header.php
+++ header.php
@@ -20,6 +20,9 @@
     <script src="<?php echo get_template_directory_uri(); ?>/scripts/selectivizr-min.js" type="text/javascript"></script>
 <![endif]-->
 
+<script src="//api.tiles.mapbox.com/mapbox.js/v1.6.2/mapbox.js"></script>
+<script src="scripts/BmoreGeo.js"></script>
+
 <?php 
 
 	if ( is_singular() ) wp_enqueue_script( 'comment-reply' );
